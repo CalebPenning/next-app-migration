1431253491654f8ab5e07685f4c2791413432276
Caleb Penning <calebpenning@mail.com>
2023-10-29 16:48:37 -0700

    rename clientlayout to more apt serverlayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css"
-import ClientLayout from "./ClientLayout"
+import ServerLayout from "./ServerLayout"
 
 export default function RootLayout({
 	children
@@ -9,7 +9,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body>
-				<ClientLayout>{children}</ClientLayout>
+				<ServerLayout>{children}</ServerLayout>
 			</body>
 		</html>
 	)
